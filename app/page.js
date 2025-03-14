4af8736a69080662833e7b60d00b250e0c285276
Rian <devriancontato@gmail.com>
2025-03-14 18:51:27 -0300

    fiz errox page.js

--- app/page.js
+++ app/page.js
@@ -2,14 +2,17 @@
 
 import { useEffect, useState } from 'react';
 import { personalData } from "@/utils/data/personal-data";
+import dynamic from "next/dynamic"; // Import para carregar componentes dinamicamente
 import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+
+// Carregar dinamicamente o componente do Blog com "ssr: false"
+const Blog = dynamic(() => import("./components/homepage/blog"), { ssr: false });
 
 // Função para buscar dados de blogs
 async function fetchBlogData() {
@@ -30,13 +33,14 @@ export default function Home() {
 
   useEffect(() => {
     async function loadData() {
-      if (typeof window !== 'undefined') {
-        const data = await fetchBlogData();
-        setBlogs(data);
-      }
+      const data = await fetchBlogData();
+      setBlogs(data);
     }
 
-    loadData();
+    // Verifica se estamos no cliente antes de carregar os dados
+    if (typeof window !== 'undefined') {
+      loadData();
+    }
   }, []);
 
   return (
