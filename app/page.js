f4c3e8fde54d0b389e5062a32e9f28391d4033c7
Rian <devriancontato@gmail.com>
2025-03-14 18:32:18 -0300

    Tentando corrigir o erro async useEffect

--- app/page.js
+++ app/page.js
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useState } from 'react';
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
@@ -8,25 +11,36 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+// Função para buscar dados de blogs
+async function fetchBlogData() {
+  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error('Failed to fetch data');
   }
 
   const data = await res.json();
-
   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
 
   return filtered;
-};
+}
 
-export default async function Home() {
-  const blogs = await getData();
+export default function Home() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    async function loadData() {
+      if (typeof window !== 'undefined') {
+        const data = await fetchBlogData();
+        setBlogs(data);
+      }
+    }
+
+    loadData();
+  }, []);
 
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
       <Experience />
@@ -36,5 +50,5 @@ export default async function Home() {
       <Blog blogs={blogs} />
       <ContactSection />
     </div>
-  )
-};
+  );
+}
